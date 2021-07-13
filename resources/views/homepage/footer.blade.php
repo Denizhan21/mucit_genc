9fdb9fa251501aa9874828a660bead1f4708448b
ebube <denizhan18@itu.edu.tr>
2021-07-13 09:27:16 +0300

    değişiklikler yapıldı

---
+++ resources/views/homepage/footer.blade.php
@@ -0,0 +1,68 @@
+{{--    Footer Başlangıç  --}}
+<footer class="footer-wrap footer-dashboard">
+
+    <div class="main-footer">
+        <div class="container">
+            <div class="row row-cols-lg-4 row-cols-sm-2 row-cols-1">
+                <div class="col">
+                    <div class="footer-box">
+                        <div class="footer-logo">
+                            <a href="{{route('homepage')}}"><img src="/homepage/media/logo_dark.png" alt="Logo"></a>
+                        </div>
+                        <p>DigitalZone Acıbadem Okulları B2RT(Bilgisayar Bilimleri ve Robotik Teknolojiler) Ekibi ve Mucit Genç Ekibi tarafından yazılım ve uygulama kısmı ile tamamen özgün , 21.yy dijital yetkinliklerini öğrencilere kazandırmak amacı ile geliştirilmiş bir projedir.</p>
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-lg-center">
+                    <div class="footer-box">
+                        <h3 class="footer-title">
+                            Kullanışlı Linkler
+                        </h3>
+                        <div class="footer-link">
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about-us.html">About us</a></li>
+                                <li><a href="shop.html">Shop</a></li>
+                                <li><a href="contact.html">Contacts</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-lg-center">
+                    <div class="footer-box">
+                        <h3 class="footer-title">
+                            İletişim
+                        </h3>
+                        <div class="footer-link">
+                            <ul>
+                                <li><a href="newsfeed.html">NewsFeed</a></li>
+                                <li><a href="user-timeline.html">Profile</a></li>
+                                <li><a href="user-friends.html">Friends</a></li>
+                                <li><a href="user-groups.html">Groups</a></li>
+                                <li><a href="forums.html">Forums</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-lg-center">
+                    <div class="footer-box">
+                        <h3 class="footer-title">
+                            Sosyal Medya Hesapları
+                        </h3>
+                        <div class="footer-link">
+                            <ul>
+                                <li><a href="https://www.facebook.com/">facebook</a></li>
+                                <li><a href="https://twitter.com/">twitter</a></li>
+                                <li><a href="https://www.instagram.com/">instagram</a></li>
+                                <li><a href="https://www.youtube.com/">Youtube</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="footer-copyright">© 2021 Denizhan Yazılım. Tüm Hakları Saklıdır.</div>
+    </div>
+</footer>
+{{--    Footer Bitiş  --}}
